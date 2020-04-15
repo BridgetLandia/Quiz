7a962645b1308faf988678d6a3a2aa63bc52c275
Brigitta <brigitta.tth@gmail.com>
2020-04-15 09:02:44 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <b-nav>
+    <b-nav-item active>Quizzz</b-nav-item>
+    <b-nav-item>Counter: {{numTotal}}/{{numCorrect}}</b-nav-item>
+  </b-nav>
+</div>
+
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
